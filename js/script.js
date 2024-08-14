c97e527177847a22ab516803c1d3d9d1ee04b9f0
nikenaahmadi <nikenauliaahmadi@gmail.com>
2024-08-14 21:19:33 +0800

    update.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Ini JavaScript
+
+console.log('linked succes')
+
+function formValidation() {
+    const inputWeight = document.getElementById('input-weight').value;
+    console.log(inputWeight);
+    console.log('form submitted');
+    
+}
